3e92099c8aca7b48af230c30ad8a1f0cdb2b1e2e
Les Orchard <me@lmorchard.com>
2022-12-11 22:17:26 -0800

    sign the get request for actor JSON, gotosocial seems to require it

--- commands/send.js
+++ commands/send.js
@@ -30,6 +30,9 @@ async function run({ from, to, message }) {
   });
 
   actors = new ActorModelStore(config);
+  
+  const fromActor = await actors.fetch(from);
+  log.debug({ msg: "fromActor", id: (await fromActor.toJSON()).id });
 
   const [username, hostname] = to.split("@", 2);
   const webfingerUrl = `https://${hostname}/.well-known/webfinger`;
@@ -44,39 +47,47 @@ async function run({ from, to, message }) {
   const { href: toActor } = links.find(
     ({ rel, type }) => rel === "self" && type === "application/activity+json"
   );
-  const { host: targetHost } = new URL(toActor);
   log.debug({ msg: "toActor", toActor });
 
+  const date = new Date();
+
   const toActorResponse = await axios.get(toActor, {
-    headers: { accept: "application/activity+json" },
+    headers: { 
+      accept: "application/activity+json", 
+      ...(await signRequest({
+        method: "get",
+        url: toActor,
+        actor: fromActor,
+        date,
+      })),
+    },
+  });
+
+  log.debug({
+    msg: "toActor",
+    data: toActorResponse.data
   });
 
   const {
     id: toActorId,
     inbox: actorInbox,
-    endpoints: { sharedInbox },
+    // gts doesn't have shared inbox
+    // endpoints: { sharedInbox },
   } = toActorResponse.data;
 
   log.debug({
     msg: "toActor inbox",
     actorInbox,
-    sharedInbox,
   });
 
-  const fromActor = await actors.fetch(from);
-  log.debug({ msg: "fromActor", id: (await fromActor.toJSON()).id });
-
   const AS_TO_PUBLIC = "https://www.w3.org/ns/activitystreams#Public";
-
-  const d = new Date();
-  const dUTC = d.toUTCString();
 
   const guidObject = crypto.randomBytes(16).toString("hex");
   let object = {
     id: `${BASE_URL}/objects/${guidObject}`,
     url: `${BASE_URL}/objects/${guidObject}`,
     type: "Note",
-    published: d.toISOString(),
+    published: date.toISOString(),
     attributedTo: fromActor.id,
     to: [AS_TO_PUBLIC],
     cc: [toActorId],
@@ -97,40 +108,19 @@ async function run({ from, to, message }) {
 
   log.debug({ msg: "activity", activity });
 
-  const {
-    href: targetInbox,
-    pathname: targetInboxPath,
-    host: targetInboxHost,
-  } = new URL(actorInbox);
-
-  const digestHash = crypto
-    .createHash("sha256")
-    .update(JSON.stringify(activity))
-    .digest("base64");
-
-  let stringToSign = `(request-target): post ${targetInboxPath}\nhost: ${targetInboxHost}\ndate: ${dUTC}\ndigest: SHA-256=${digestHash}`;
-
-  const signer = crypto.createSign("sha256");
-  signer.update(stringToSign);
-  signer.end();
-
-  const privkey = await fromActor.privateKey();
-  const signature = signer.sign(privkey);
-  const signatureB64 = signature.toString("base64");
-  const header = `keyId="${fromActor.keyId}",headers="(request-target) host date digest",signature="${signatureB64}"`;
-
-  log.debug({ msg: "signature", stringToSign, signatureB64, header });
-
   try {
     const inboxResponse = await axios({
       method: "post",
-      url: targetInbox,
+      url: actorInbox,
       data: activity,
       headers: {
-        Host: targetHost,
-        Date: dUTC,
-        Digest: `SHA-256=${digestHash}`,
-        Signature: header,
+        ...(await signRequest({
+          method: "post",
+          url: actorInbox,
+          actor: fromActor,
+          date,
+          content: JSON.stringify(activity),
+        })),
       },
     });
 
@@ -140,6 +130,7 @@ async function run({ from, to, message }) {
       data: inboxResponse.data,
     });
   } catch (error) {
+    console.log(error);
     log.error({
       msg: "inboxResponse",
       error,
@@ -147,4 +138,50 @@ async function run({ from, to, message }) {
       // data: error.data,
     });
   }
+}
+
+async function signRequest({ actor, method, url, date, content }) {
+  const { pathname, host } = new URL(url);
+  const dateUTC = date.toUTCString();
+
+  const partsToSign = [
+    `(request-target): ${method} ${pathname}`,
+    `host: ${host}`,
+    `date: ${dateUTC}`,
+  ];
+
+  const headers = {
+    Host: host,
+    Date: dateUTC,
+  };
+  const headersList = ["host", "date"];
+
+  if (content) {
+    const digestHash = crypto
+      .createHash("sha256")
+      .update(content)
+      .digest("base64");
+    partsToSign.push(`digest: SHA-256=${digestHash}`);
+    headers["Digest"] = `SHA-256=${digestHash}`;
+    headersList.push("digest");
+  }
+
+  const stringToSign = partsToSign.join("\n");
+
+  const signer = crypto.createSign("sha256");
+  signer.update(stringToSign);
+  signer.end();
+
+  const privkey = await actor.privateKey();
+  const signature = signer.sign(privkey);
+  const signatureB64 = signature.toString("base64");
+
+  headers.Signature = [
+    `keyId="${actor.keyId}"`,
+    `headers="(request-target) ${headersList.join(" ")}"`,
+    `signature="${signatureB64}"`,
+  ].join(",");
+
+  console.log("HEADERS", headers);
+  return headers;
 }
