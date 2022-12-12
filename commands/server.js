d86297083e2152780fd0ab9a42d4d5960a2fc02b
Les Orchard <me@lmorchard.com>
2022-12-11 17:00:30 -0800

    refactor into index.js with multiple commands

---
+++ commands/server.js
@@ -0,0 +1,135 @@
+/*
+TODO:
+- webfinger lookup
+- actor JSON
+- inbox endpoint
+- outbox feed
+- outgoing POST queue
+- c2s API?
+*/
+import path from "path";
+import { URL } from "url";
+
+import Fastify from "fastify";
+import FastifyStatic from "@fastify/static";
+import FastifyAccepts from "@fastify/accepts";
+import Boom from "@hapi/boom";
+
+import config from "../lib/config.js";
+import { ActorModelStore } from "../lib/models.js";
+
+let server;
+let actors;
+
+export default function init({ program }) {
+  program.command("server").description("run server").action(run);
+}
+
+async function run() {
+  const { LOG_LEVEL, PUBLIC_PATH, HOST, PORT } = config;
+  actors = new ActorModelStore(config);
+
+  server = Fastify({
+    logger: { level: LOG_LEVEL },
+  });
+
+  server
+    .register(FastifyStatic, {
+      root: path.resolve(PUBLIC_PATH),
+      prefix: "/",
+    })
+    .register(FastifyAccepts);
+
+  // TODO: split this up into plugins - e.g. to use hooks to check :actor existence across all handlers
+  server
+    .get("/.well-known/webfinger", getWebFinger)
+    .get("/actors/:username/icon", getActorIcon)
+    .get("/actors/:username", getActor)
+    .post("/inbox", postSharedInbox)
+    .post("/actors/:username/inbox", postActorInbox);
+
+  server.listen({ host: HOST, port: PORT });
+}
+
+// e.g. acct:user@example.com
+const RE_WEBFINGER = new RegExp(
+  "(?<scheme>[^:]+):(?<username>[^@]+)@(?<hostname>.+)"
+);
+
+async function getWebFinger(request, reply) {
+  const { BASE_URL } = config;
+  const { resource } = request.query;
+
+  const match = RE_WEBFINGER.exec(resource);
+  if (!match) {
+    return reply.send(Boom.notAcceptable("unacceptable webfinger query"));
+  }
+
+  const { scheme, username, hostname } = match.groups;
+
+  const siteDomain = new URL(BASE_URL).host;
+  if (scheme !== "acct" || hostname !== siteDomain) {
+    return reply.send(Boom.notAcceptable("unacceptable scheme or hostname"));
+  }
+
+  const exists = await actors.exists(username);
+  if (!exists) {
+    return reply.send(Boom.notFound("username not found"));
+  }
+
+  const webfingerResponse = {
+    subject: resource,
+    links: [
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: `${BASE_URL}/actors/${username}`,
+      },
+    ],
+  };
+
+  return reply
+    .code(200)
+    .headers({
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/jrd+json",
+    })
+    .send(webfingerResponse);
+}
+
+async function getActor(request, reply) {
+  const { username } = request.params;
+
+  const exists = await actors.exists(username);
+  if (!exists) {
+    return reply.send(Boom.notFound("actor not found"));
+  }
+
+  const actor = await actors.fetch(username);
+
+  const accept = request.accepts();
+  switch (accept.type(["json", "html"])) {
+    case "json":
+      return reply.type("application/activity+json").send(await actor.toJSON());
+    case "html":
+      reply.type("text/html").send("<b>hello, world!</b>");
+      break;
+  }
+}
+
+async function getActorIcon(request, reply) {
+  const { username } = request.params;
+
+  const exists = await actors.exists(username);
+  if (!exists) {
+    return reply.send(Boom.notFound("actor not found"));
+  }
+
+  const actor = await actors.fetch(username);
+  // TOOD: serving this file up directly like this is a little dicey
+  return reply.sendFile(actor.iconFilePath, { root: "/" });
+}
+
+async function postSharedInbox(request, reply) {}
+
+async function postActorInbox(request, reply) {}
--- index.js
+++ index.js
@@ -1,132 +1,15 @@
-/*
-TODO:
-- webfinger lookup
-- actor JSON
-- inbox endpoint
-- outbox feed
-- outgoing POST queue
-- c2s API?
-*/
-import path from "path";
-import { URL } from "url";
+#!/usr/bin/env node
+import { Command } from "commander";
 
-import Fastify from "fastify";
-import FastifyStatic from "@fastify/static";
-import FastifyAccepts from "@fastify/accepts";
-import Boom from "@hapi/boom";
+import { init as initConfig } from "./lib/config.js";
+import initServer from "./commands/server.js";
 
-import Config from "./lib/config.js";
-import { ActorModelStore } from "./lib/models.js";
-
-let server;
-let actors;
+const program = new Command();
+const context = { program };
+[initConfig, initServer].forEach((fn) => fn(context));
 
 async function main() {
-  const { LOG_LEVEL, PUBLIC_PATH, HOST, PORT } = Config;
-  actors = new ActorModelStore(Config);
-
-  server = Fastify({
-    logger: { level: LOG_LEVEL },
-  });
-
-  server
-    .register(FastifyStatic, {
-      root: path.resolve(PUBLIC_PATH),
-      prefix: "/",
-    })
-    .register(FastifyAccepts);
-
-  server
-    .get("/.well-known/webfinger", getWebFinger)
-    .get("/actors/:username/icon", getActorIcon)
-    .get("/actors/:username", getActor)
-    .post("/inbox", postSharedInbox)
-    .post("/actors/:username/inbox", postActorInbox);
-
-  server.listen({ host: HOST, port: PORT });
+  await program.parseAsync(process.argv);
 }
 
-// e.g. acct:user@example.com
-const RE_WEBFINGER = new RegExp(
-  "(?<scheme>[^:]+):(?<username>[^@]+)@(?<hostname>.+)"
-);
-
-async function getWebFinger(request, reply) {
-  const { BASE_URL } = Config;
-  const { resource } = request.query;
-
-  const match = RE_WEBFINGER.exec(resource);
-  if (!match) {
-    return reply.send(Boom.notAcceptable("unacceptable webfinger query"));
-  }
-
-  const { scheme, username, hostname } = match.groups;
-
-  const siteDomain = new URL(BASE_URL).host;
-  if (scheme !== "acct" || hostname !== siteDomain) {
-    return reply.send(Boom.notAcceptable("unacceptable scheme or hostname"));
-  }
-
-  const exists = await actors.exists(username);
-  if (!exists) {
-    return reply.send(Boom.notFound("username not found"));
-  }
-
-  const webfingerResponse = {
-    subject: resource,
-    links: [
-      {
-        rel: "self",
-        type: "application/activity+json",
-        href: `${BASE_URL}/actors/${username}`,
-      },
-    ],
-  };
-
-  return reply
-    .code(200)
-    .headers({
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/jrd+json",
-    })
-    .send(webfingerResponse);
-}
-
-async function getActor(request, reply) {
-  const { username } = request.params;
-
-  const exists = await actors.exists(username);
-  if (!exists) {
-    return reply.send(Boom.notFound("actor not found"));
-  }
-
-  const actor = await actors.fetch(username);
-
-  const accept = request.accepts();
-  switch (accept.type(["json", "html"])) {
-    case "json":
-      return reply.type("application/activity+json").send(await actor.toJSON());
-    case "html":
-      reply.type("text/html").send("<b>hello, world!</b>");
-      break;
-  }
-}
-
-async function getActorIcon(request, reply) {
-  const { username } = request.params;
-
-  const exists = await actors.exists(username);
-  if (!exists) {
-    return reply.send(Boom.notFound("actor not found"));
-  }
-
-  const actor = await actors.fetch(username);
-  // TOOD: serving this file up directly like this is a little dicey
-  return reply.sendFile(actor.iconFilePath, { root: "/" });
-}
-
-async function postSharedInbox(request, reply) {}
-
-async function postActorInbox(request, reply) {}
-
-main().catch(console.error);
+main().catch((err) => console.error(err));
